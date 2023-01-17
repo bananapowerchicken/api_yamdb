49b66f9d05abda4c0be9eea0039851020f69a639
Анна МительковаÐ <221882@mail.ru>
2023-01-17 21:39:23 +0300

    add role and bio fields to user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class UserRole(models.TextChoices):
+        USER = 'user',
+        MODERATOR = 'moderator',
+        ADMIN = 'admin'
+
+    role = models.CharField(
+        choices=UserRole.choices,
+        default=UserRole.USER,
+    )
+
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
