1ef08cba6a3475a470ea9a9fb52895bafeb926e2
Alina Almukhametova <almukhametovaalina@gmail.com>
2023-01-18 13:15:28 +0800

    first-review-model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,34 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    """Модель отзыва."""
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField(
+        'Текст отзыва'
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор отзыва',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField(
+        'Оценка',
+        help_text='Оцените произведение от 1 до 10',
+        validators=[
+            MinValueValidator(1, message='Минимальная оценка - 1'),
+            MaxValueValidator(10, message='Максимальная оценка - 10')
+        ]
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления отзыва',
+        auto_now_add=True,
+        db_index=True
+    )
