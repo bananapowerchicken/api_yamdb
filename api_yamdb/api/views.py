3b48aaecfb516b71340d1c30203591ad530695d5
Анна МительковаÐ <221882@mail.ru>
2023-01-19 15:56:55 +0300

    removed unexisting permission

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 
 from reviews.models import CustomUser
 from .serializers import CustomUserSerializer
-from rest_framework.permissions import IsAdmin
 
 
 class UserViewSet(viewsets.ModelViewSet):
