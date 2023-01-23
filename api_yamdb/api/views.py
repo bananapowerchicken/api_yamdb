ffb7e71ddb5041d568e46b6e7877c90cac2f1507
Анна МительковаÐ <221882@mail.ru>
2023-01-23 16:55:29 +0300

    add /me/ address

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -62,34 +62,34 @@ def get_user_token(request):
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    http_method_names = ['get', 'patch', 'delete']
+    http_method_names = ['post', 'get', 'patch', 'delete']
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'username'
     permission_classes = (IsAdmin,)
 
-    # @action(
-    #     methods=[
-    #         "get",
-    #         "patch",
-    #     ],
-    #     detail=False,
-    #     url_path="me",
-    #     permission_classes=[permissions.IsAuthenticated],
-    #     serializer_class=UserEditSerializer,
-    # )
-    # def users_own_profile(self, request):
-    #     user = request.user
-    #     if request.method == "GET":
-    #         serializer = self.get_serializer(user)
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     if request.method == "PATCH":
-    #         serializer = self.get_serializer(
-    #             user,
-    #             data=request.data,
-    #             partial=True
-    #         )
-    #         serializer.is_valid(raise_exception=True)
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    @action(
+        methods=[
+            "get",
+            "patch",
+        ],
+        detail=False,
+        url_path="me",
+        permission_classes=[permissions.IsAuthenticated],
+        serializer_class=UserEditSerializer,
+    )
+    def users_own_profile(self, request):
+        user = request.user
+        if request.method == "GET":
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        if request.method == "PATCH":
+            serializer = self.get_serializer(
+                user,
+                data=request.data,
+                partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
