3a11b7480551fa3aa47cb820e3489840e1d40ed0
Анна МительковаÐ <221882@mail.ru>
2023-01-28 23:31:47 +0300

    ready to put to dev

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -47,21 +47,12 @@ class UserEditSerializer(serializers.ModelSerializer):
         read_only_fields = ('role',)
 
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     """Сериализатор для работы с категориями."""
-
-#     class Meta:
-#         model = Category
-#         fields = ('name', 'slug')
 class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор для работы с категориями."""
 
     class Meta:
         model = Category
-        exclude = ('id',)
-        lookup_field = 'slug'
-        extra_kwargs = {
-            'url': {'lookup_field': 'slug'}
-        }
+        fields = ('name', 'slug')
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -79,7 +70,7 @@ class TitleSerializer(serializers.ModelSerializer):
     genre = serializers.SlugRelatedField(
         queryset=Genre.objects.all(), slug_field='slug', many=True
     )
-    rating = serializers.SerializerMethodField()
+    # rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Title
@@ -87,11 +78,24 @@ class TitleSerializer(serializers.ModelSerializer):
             'id',
             'name',
             'year',
-            'rating',
             'description',
             'category',
             'genre',
         )
+
+
+class TitleSerializerRead(serializers.ModelSerializer):
+    """Сериализатор для работы с произведениями при чтении."""
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(many=True, read_only=True)
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'description', 'year', 'category', 'genre', 'rating'
+        )
+        read_only_fields = ('id',)
 
     def get_rating(self, obj):
         score_sum = Review.objects.filter(title_id=obj.id).aggregate(
@@ -103,20 +107,6 @@ class TitleSerializer(serializers.ModelSerializer):
         rating = score_sum / score_count
         rating = int(rating + (0.5 if rating > 0 else -0.5))
         return round(rating)
-
-
-class TitleSerializerRead(serializers.ModelSerializer):
-    """Сериализатор для работы с произведениями при чтении."""
-    category = CategorySerializer(read_only=True)
-    genre = GenreSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Title
-        fields = (
-            'id', 'name', 'description', 'year', 'category', 'genre'
-        )
-        read_only_fields = ('id',)
-
 
 class TitleSerializerCreate(serializers.ModelSerializer):
     """Сериализатор для работы с произведениями при создании."""
