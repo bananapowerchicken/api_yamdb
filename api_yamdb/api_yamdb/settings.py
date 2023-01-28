734e612278f6394b619e1bd26cb3b57a5761a784
Анна МительковаÐ <221882@mail.ru>
2023-01-29 00:41:53 +0300

    екн ещ ашч

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,8 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'django_filters',
+    'rest_framework_simplejwt',    
     'api',
     'reviews',
 ]
